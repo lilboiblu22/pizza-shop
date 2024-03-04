54cdd394462ab900387546ab2aca1c88841a1347
Enrique Jimenez <ejimene@iu.edu>
2024-03-04 18:44:51 -0500

    Adding files for NewYorkStore

---
+++ src/main/java/edu/iu/habahram/pizzashop/model/ThinCrustDough.java
@@ -0,0 +1,8 @@
+package edu.iu.habahram.pizzashop.model;
+
+public class ThinCrustDough implements Dough{
+
+    public String toString() {
+        return "ThinCrust style extra thin crust dough";
+    }
+}
